686c7a8d7b53da48cf26f46e80f0d03ed297c73d
mathysfrc <mathysf41@gmail.com>
2023-11-10 12:05:32 +0100

    modifs

--- Desktop/inventaire/migrations/Version20231109135522.php
+++ Desktop/inventaire/migrations/Version20231109135522.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230904083700 extends AbstractMigration
+final class Version20231109135522 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20230904083700 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE stock ADD surface VARCHAR(255) DEFAULT NULL, ADD shape VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE tracking ADD surface VARCHAR(255) DEFAULT NULL, ADD shape VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE user');
+        $this->addSql('ALTER TABLE tracking DROP surface, DROP shape');
+        $this->addSql('ALTER TABLE stock DROP surface, DROP shape');
     }
 }
--- Desktop/inventaire/src/Controller/TableExportController.php
+++ Desktop/inventaire/src/Controller/TableExportController.php
@@ -26,18 +26,18 @@ class TableExportController extends AbstractController
             // Récupérez les données de la table que vous souhaitez exporter (par exemple, à partir d'une entité Doctrine).
         
             // Formattez les données dans une chaîne de caractères txt.
-            $txtContent = "SKU\tDescription\tDimension\tRésultat\tPrix\tFamille produit\tRéférence\tStatus\n"; 
+            $txtContent = "SKU\tDescription\tDimension\tRésultat\tPrix\tFamille produit\tRéférence\tStatus\tAire\tForme\n"; 
             
             foreach ($donnees as $item) {
                 $txtContent .= $item->getSKU() . "\t" . $item->getDescription() . "\t" . $item->getSize1Name() . ' : ' . $item->getSize1() . ' ' .  $item->getSize1Unit() . ' x '  . $item->getSize2Name() . ' : ' . $item->getSize2() . ' ' . $item->getSize2Unit()  . "\t";
             
                 if ($item->getSize1Unit() == "mm"  && $item->getSize2Unit() == "mm") {
-                    $txtContent .= ($item->getSize1() * $item->getSize2()) / 1000000 . ' m² ';
+                    $txtContent .= ($item->getSurface() === null ? $item->getSurface() : ($item->getSize1() * $item->getSize2() / 1000000 )) . ' m² ';
                 } elseif ($item->getSize1Unit() == 'unité'  && $item->getSize2Unit() == 'L') {
-                    $txtContent .= ($item->getSize1() * $item->getSize2()) . ' L ';
+                    $txtContent .= ($item->getSurface() === null ? $item->getSurface() : ($item->getSize1() * $item->getSize2() / 1000000 )) . ' L ';
                 }
             
-                $txtContent .= "\t" . $item->getPrice() .  ' € '  .  "\t" . $item->getProductFamily() .  "\t" . $item->getReference() . "\t" . $item->getStatus() . "\n";
+                $txtContent .= "\t" . $item->getPrice() .  ' € '  .  "\t" . $item->getProductFamily() .  "\t" . $item->getReference() . "\t" . $item->getStatus() . "\t" . $item->getSurface() . "\t" . $item->getShape() . "\n";
             }
         
             // Créez une réponse Symfony avec le contenu du fichier txt.
